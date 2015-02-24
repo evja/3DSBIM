005810c2e57f8fff1360952f15cf8ce2bed06230
Evan A <evjay.a@gmail.com>
2015-02-24 10:35:28 -0700

    added the project controller CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,13 +1,51 @@
 class ProjectsController < ApplicationController
+
   def show
+    @project = Project.find(params[:id])
   end
 
   def index
+    @projects = Project.all
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.build(project_params)
+    if @project.save
+      respond_to do |format|
+        format.html redirect_to @project
+        format.json
+      end
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @project.update_attributes(project_params)
+      respond_to do |format|
+        format.html redirect_to @project
+        format.json
+      end
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.delete
+    redirect_to @projects
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :desription)
   end
 end
